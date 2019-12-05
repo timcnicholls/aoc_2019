2744504b0e95333add535ce7c0432506bd33aee0
Tim Nicholls <tim.nicholls@stfc.ac.uk>
2019-12-05 14:30:32 +0000

    Tidier version of day 5 code

--- day_5.py
+++ day_5.py
@@ -2,6 +2,8 @@
 
 import enum
 import logging
+import operator
+import sys
 
 class IntCodeProcessor(object):
 
@@ -32,8 +34,8 @@ class IntCodeProcessor(object):
             self.Opcode.MULTIPLY:   (self._multiply, 4),
             self.Opcode.INPUT:      (self._input, 2),
             self.Opcode.OUTPUT:     (self._output, 2),
-            self.Opcode.JUMP_TRUE:  (self._jump_true, 0),
-            self.Opcode.JUMP_FALSE: (self._jump_false, 0),
+            self.Opcode.JUMP_TRUE:  (self._jump_true, 3),
+            self.Opcode.JUMP_FALSE: (self._jump_false, 3),
             self.Opcode.LESS_THAN:  (self._less_than, 4),
             self.Opcode.EQUALS:     (self._equals, 4),
             self.Opcode.HALT:       (self._halt, 1),
@@ -79,32 +81,46 @@ class IntCodeProcessor(object):
 
     def run(self):
         
-        self.instruction_ptr = 0
-        self.outputs = []
-
         logging.debug('Running program of length {}'.format(self.memory_len))
         
-        running = True
+        self.instruction_ptr = 0
+        self.outputs = []
+        self.running = True
         num_instructions = 0
 
-        while running and (self.instruction_ptr < self.memory_len):
+        # Loop over the instructions in memory while HALT has not been encountered and the
+        # instruction pointer is within bounds
 
+        while self.running and (self.instruction_ptr < self.memory_len):
+
+            # Get the next opcode and parameter modes from memory
             (opcode, param_modes) = self._parse_instruction(self.memory[self.instruction_ptr])
-            logging.debug("  ptr {} mem {} opcode {} param_modes {}".format(
-                self.instruction_ptr, self.memory[self.instruction_ptr], opcode, param_modes
-            ))
 
+            # Attempt to resolve the instruction function and length from the opcode dict
             try:
                 (instruction, instruction_len) = self.instructions[opcode]
-                running = instruction(self.instruction_ptr, param_modes)
             except KeyError:
                 raise RuntimeError(
                     "Invalid opcode {} at instruction pointer {}".format(
                     opcode, self.instruction_ptr
                 ))
+
+            # Extract the instruction parameter list from memory
+            params = self.memory[self.instruction_ptr+1:self.instruction_ptr+instruction_len]
+
+            logging.debug("****: ptr {} mem {} opcode {} params {} param_modes {}".format(
+                self.instruction_ptr, self.memory[self.instruction_ptr], 
+                opcode, params, param_modes
+            ))
+
+            # Execute the instruction
+            instruction(*params, param_modes)
+
+            # Increment the instruction pointer by the appropriate length
             self.instruction_ptr += instruction_len
             num_instructions += 1
 
+        # Run complete, set the output parameter to the conents of the first memory position
         output = self.memory[0]
         logging.debug("Run completed after {} instructions with output {}".format(
             num_instructions, output
@@ -125,133 +141,87 @@ class IntCodeProcessor(object):
     
         return (opcode, param_modes)
 
-    def _resolve_params(self, ptr, num_params, param_modes):
+    def _resolve_params(self, params, param_modes):
         
-        params = []
-        for idx in range(num_params):
-            pos_param = self.memory[ptr + 1 + idx]
-            if param_modes[idx] == self.ParamMode.POSITION:
-                params.append(self.memory[pos_param])
-            elif param_modes[idx] == self.ParamMode.IMMEDIATE:
-                params.append(pos_param)
+        if len(params) > len(param_modes):
+            raise RuntimeError("Attempting to resolve too many parameters")
 
-        return params
+        def resolve(param, mode):
+            if mode == self.ParamMode.POSITION:
+                return self.memory[param]
+            else:
+                return param
 
-    def _add(self, ptr, param_modes):
+        return list(map(resolve, params, param_modes))
 
-        assert self.ParamMode.POSITION == param_modes[2]
+    def _add(self, input_1, input_2, output, param_modes):
 
-        params = self._resolve_params(ptr, 2, param_modes)
-        pos_result = self.memory[ptr+3]
+        self._operator(input_1, input_2, output, param_modes, operator.add)
 
-        self.memory[pos_result] = params[0] + params[1]
+    def _multiply(self, input_1, input_2, output, param_modes):
 
-        logging.debug("  ADD : [{}]+[{}]=[{}] : {}+{}={}".format(
-            ptr+1, ptr+2, pos_result,
-            params[0], params[1], self.memory[pos_result]
-        ))
-        return True
+        self._operator(input_1, input_2, output, param_modes, operator.mul)
 
-    def _multiply(self, ptr, param_modes):
+    def _less_than(self, input_1, input_2, output, param_modes):
 
-        assert self.ParamMode.POSITION == param_modes[2]
+        self._operator(input_1, input_2, output, param_modes, operator.lt)
 
-        params = self._resolve_params(ptr, 2, param_modes)
-        pos_result = self.memory[ptr+3]
-        result =params[0] * params[1]
+    def _equals(self, input_1, input_2, output, param_modes):
 
-        logging.debug("  MULT: [{}]+[{}]=[{}] : {}+{}={}".format(
-            ptr+1, ptr+2, pos_result,
-            params[0], params[1], result
-        ))
-        self.memory[pos_result] = result
+        self._operator(input_1, input_2, output, param_modes, operator.eq)
 
-        return True
+    def _operator(self, input_1, input_2, output, param_modes, operation):
 
-    def _input(self, ptr, param_modes):
-
-        input_val = self.inputs.pop(0)
-        pos_input = self.memory[ptr + 1]
-        logging.debug("  INPUT: ptr {} [{}]={}".format(ptr, pos_input, input_val))
-        self.memory[pos_input] = input_val
-
-        return True
-
-    def _output(self, ptr, param_modes):
+        (value_1, value_2) = self._resolve_params((input_1, input_2), param_modes)
+        self.memory[output] = int(operation(value_1,value_2))
         
-        output_val = self._resolve_params(ptr, 1, param_modes)[0]
-        logging.debug("  OUTPUT: ptr {} [{}]={}".format(ptr, ptr+1, output_val))
-        self.outputs.append(output_val)
+        logging.debug(">>>>: {} : [{}]*[{}]=[{}] : {}*{}={}".format(
+            operation.__name__.upper(), input_1, input_2, output, 
+            value_1, value_2, self.memory[output]
+        ))
 
-        return True
+    def _input(self, input_ptr, param_modes):
 
-    def _jump_true(self, ptr, param_modes):
+        input_value = self.inputs.pop(0)
+        logging.debug(">>>>: INPUT: [{}]={}".format(input_ptr, input_value))
+        self.memory[input_ptr] = input_value
+
+    def _output(self, output_param, param_modes):
         
-        self._jump_condition(ptr, param_modes, True)
-        return True
+        output_value = self._resolve_params([output_param], param_modes)[0]
+        logging.debug(">>>>: OUTPUT: [{}]={}".format(output_param, output_value))
+        self.outputs.append(output_value)
 
-    def _jump_false(self, ptr, param_modes):
+    def _jump_true(self, input_val, input_jump, param_modes):
+        
+        self._jump_condition(input_val, input_jump, param_modes, True)
 
-        self._jump_condition(ptr, param_modes, False)
-        return True
+    def _jump_false(self, input_val, input_jump, param_modes):
 
-    def _jump_condition(self, ptr, param_modes, condition):
+        self._jump_condition(input_val, input_jump, param_modes, False)
 
-        params = self._resolve_params(ptr, 2, param_modes)
-        logging.debug("  JUMP_{}: ptr {} val {} ins_ptr {}".format(
-            str(condition).upper(), ptr, params[0], params[1]
+    def _jump_condition(self, input_val, input_jump, param_modes, condition):
+
+        (value, jump_ptr) = self._resolve_params((input_val, input_jump), param_modes)
+        logging.debug(">>>>: JUMP_{}: [{}] val {} ins_ptr {}".format(
+            str(condition).upper(), input, value, jump_ptr,
         ))
 
-        if (params[0] != 0) == condition: 
-            self.instruction_ptr = params[1]
-        else:
-            self.instruction_ptr += 3
-        pass
+        if (value != 0) == condition:             
+            self.instruction_ptr = jump_ptr - 3
 
-    def _less_than(self, ptr, param_modes):
+    def _halt(self, param_modes):
+        logging.debug(">>>>: HALT")
+        self.running = False
 
-        assert self.ParamMode.POSITION == param_modes[2]
-
-        params = self._resolve_params(ptr, 2, param_modes)
-        pos_result = self.memory[ptr+3]
-
-        self.memory[pos_result] = int(params[0] < params[1])
-
-        logging.debug("  LESS THAN : [{}]=[{}]=[{}] : {}+{}={}".format(
-            ptr+1, ptr+2, pos_result,
-            params[0], params[1], self.memory[pos_result]
-        ))
-
-        return True
-
-    def _equals(self, ptr, param_modes):
-
-        assert self.ParamMode.POSITION == param_modes[2]
-
-        params = self._resolve_params(ptr, 2, param_modes)
-        pos_result = self.memory[ptr+3]
-
-        self.memory[pos_result] = int(params[0] == params[1])
-
-        logging.debug("  EQUALS : [{}]=[{}]=[{}] : {}+{}={}".format(
-            ptr+1, ptr+2, pos_result,
-            params[0], params[1], self.memory[pos_result]
-        ))
-
-        return True
-
-    def _halt(self, ptr, param_modes):
-        logging.debug("  HALT: ptr {}".format(ptr))
-        return False
-
-    def run_self_test_cases(self, test_cases, test_results=[], test_inputs=[], test_outputs=[]):
+    def run_self_test_cases(self, name, test_cases, test_results=[], test_inputs=[], test_outputs=[]):
 
         if len(test_results) == 0:
-            test_results = []*len(test_cases)
+            test_results = [[]]*len(test_cases)
         if len(test_inputs) == 0:
-            test_inputs = []*len(test_cases)
+            test_inputs = [[]]*len(test_cases)
         if len(test_outputs) == 0:
-            test_outputs = []*len(test_cases)
+            test_outputs = [[]]*len(test_cases)
 
         for (test_case, test_result, test_input, test_output) in zip(
             test_cases, test_results, test_inputs, test_outputs):
@@ -266,9 +236,12 @@ class IntCodeProcessor(object):
             if len(test_output):
                 assert test_output == self.get_outputs()
 
+        logging.info("{} test cases completed OK".format(name))
+
     def self_test_basic(self):
 
         test_cases = [
+            [1,5,6,3,99,30,40],
             [1,9,10,3,2,3,11,0,99,30,40,50],
             [1,0,0,0,99] ,
             [2,3,0,3,99],
@@ -277,6 +250,7 @@ class IntCodeProcessor(object):
         ]
 
         test_results = [
+            [1,5,6,70,99,30,40],
             [3500,9,10,70,2,3,11,0,99,30,40,50],
             [2,0,0,0,99],
             [2,3,0,6,99],
@@ -284,7 +258,7 @@ class IntCodeProcessor(object):
             [30,1,1,4,2,5,6,0,99]
         ]
 
-        self.run_self_test_cases(test_cases, test_results)
+        self.run_self_test_cases('Basic', test_cases, test_results)
 
     def self_test_part1(self):
 
@@ -310,7 +284,7 @@ class IntCodeProcessor(object):
             [1234,0,4,0,99],
             [1002,4,3,4,99]
         ]
-        self.run_self_test_cases(test_cases, test_results, test_inputs, test_outputs)
+        self.run_self_test_cases('Part 1', test_cases, test_results, test_inputs, test_outputs)
 
     def self_test_part2(self):
 
@@ -338,9 +312,9 @@ class IntCodeProcessor(object):
             [7],
             [9],
             [0],
-            [17],
+            [178],
             [0],
-            [17],
+            [179],
         ]
         test_outputs = [
             [1],
@@ -366,34 +340,52 @@ class IntCodeProcessor(object):
             [3,3,1107,1,8,3,4,3,99],
             [3,3,1107,0,8,3,4,3,99],
             [3,12,6,12,15,1,13,14,13,4,13,99,0,0,1,9],
-            [3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, 17, 1, 1, 9],
+            [3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, 178, 1, 1, 9],
             [3, 3, 1105, 0, 9, 1101, 0, 0, 12, 4, 12, 99, 0],
-            [3, 3, 1105, 17, 9, 1101, 0, 0, 12, 4, 12, 99, 1],
+            [3, 3, 1105, 179, 9, 1101, 0, 0, 12, 4, 12, 99, 1],
         ]
-        self.run_self_test_cases(test_cases, test_results, test_inputs, test_outputs)
+        self.run_self_test_cases('Part 2a', test_cases, test_results, test_inputs, test_outputs)
 
-        test_cases = [
+        test_cases = [[
             3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
             1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
             999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99
-        ]*3
-
+        ]]*3
         test_inputs = [[7], [8], [9]]
 
         test_outputs = [[999], [1000], [1001]]
 
-        test_results = []
-        self.run_self_test_cases(test_cases, test_results, test_inputs, test_outputs)
+        test_results = [
+            [
+                3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+                1106,0,36,98,0,7,1002,21,125,20,4,20,1105,1,46,104,
+                999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99
+            ],
+            [
+                3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+                1106,0,36,98,1000,8,1002,21,125,20,4,20,1105,1,46,104,
+                999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99
+            ],
+            [
+                3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+                1106,0,36,98,1001,9,1002,21,125,20,4,20,1105,1,46,104,
+                999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99
+            ]
+        ]
+        self.run_self_test_cases('Part 2b', test_cases, test_results, test_inputs, test_outputs)
+
+def run_part_with_inputs(proc, inputs):
+
+    proc.reset_memory()
+    proc.load_inputs(inputs)
+    proc.run()
+    return proc.get_outputs()
 
 def part1(proc):
 
     proc.reset_memory()
 
-    inputs = [1]
-    proc.load_inputs(inputs)
-    proc.run()
-    outputs = proc.get_outputs()
-
+    outputs = run_part_with_inputs(proc, [1])
     outcome = "PASSED" if sum(outputs[:-1]) == 0 else "FAILED"
     logging.info("Part 1: TEST diagnostic {} with code {}".format(
         outcome, outputs[-1]
@@ -403,18 +395,23 @@ def part2(proc):
     
     proc.reset_memory()
     
-    inputs = [5]
-    proc.load_inputs(inputs)
-    proc.run()
-    diagnostic_code = proc.get_outputs()[0]
-    logging.info("Part 2: Test diagnostic produced code {}".format(
-        diagnostic_code
+    outputs = run_part_with_inputs(proc, [5])
+    outcome = "PASSED" if len(outputs) == 1 else "FAILED"
+    logging.info("Part 2: TEST diagnostic {} with code {}".format(
+        outcome, outputs[0]
     ))
 
 def main():
 
+    log_level = logging.INFO
+    try:
+        if int(sys.argv[1]):
+            log_level = logging.DEBUG
+    except (ValueError, IndexError):
+        pass
+
     logging.basicConfig(
-        level=logging.INFO, format='%(levelname)-8s: %(message)s', datefmt='%H:%M:%S'
+        level=log_level, format='%(levelname)-8s: %(message)s', datefmt='%H:%M:%S'
     )
 
     proc = IntCodeProcessor()
